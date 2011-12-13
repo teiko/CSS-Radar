ff117aacf11ee0ae0aff767c344c941bab465169
Yuya Saito <studiomohawk@gmail.com>
2011-12-14 00:13:16 +0900

    About site and me update

--- _site/js/common.js
+++ _site/js/common.js
@@ -8,21 +8,3 @@ $(document).ready(function() {
 	});
 // END
 });
-(function() {
-	var s = document.getElementsByTagName("script")[0], rdb = document.createElement("script");
-	rdb.type = "text/javascript";
-	rdb.async = true;
-	rdb.src = document.location.protocol + "//www.readability.com/embed.js"; s.parentNode.insertBefore(rdb, s);
-})();
-(function(){
-	var twitterWidgets = document.createElement('script');
-	twitterWidgets.type = 'text/javascript';
-	twitterWidgets.async = true;
-	twitterWidgets.src = 'http://platform.twitter.com/widgets.js';
-	document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
-})();
-(function() {
-	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-	po.src = 'https://apis.google.com/js/plusone.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
